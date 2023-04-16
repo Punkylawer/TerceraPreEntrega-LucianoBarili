b5920826cfa34e7ba598bbd822f1355abf4b67a2
Luciano Barili <luciano.barili@gmail.com>
2023-04-16 18:40:03 -0300

    cambios re entrega

--- js/prestamos.js
+++ js/prestamos.js
@@ -4,8 +4,17 @@ window.addEventListener('load', function() {
     setPage();
     });
 
+/* Variables globales */
+let datosAlmacenados = JSON.parse(localStorage.getItem('prestamos'));
 let prestamos = [];
+let carritoPrestamos = [];
+let totalMontoPrestamos = 0;
+let totalMontoDevolucion = 0;
+let tabla;
+let formu;
 
+/* Función para Fetch */
+function datosPrestamosJSon() {
 fetch("./datosPrestamos.json")
     .then((response) => {
         if (!response.ok) {
@@ -21,47 +30,93 @@ fetch("./datosPrestamos.json")
     .catch((error) => {
         console.error("Error en la carga de datos:", error);
     });
+}
 
-
-//Creo una funcion donde utilizo DOM. Creo clases de bootstrap. 
+/* Función para manipular DOM */
 function setPage() {
-let formu = document.getElementById('div1');
-formu.classList.add('container');
-formu.classList.add('row');
-formu.classList.add('justify-content-around');
-
-//También traigo los datos del array préstamos de "script.js" creando una variable a tales fines. Esto era de la 3ra entrega. Ahora uso json.
-let datosAlmacenados = JSON.parse(localStorage.getItem('prestamos'));
-
-//Creo el carrito y las variables que uso en el bucle de la funcion recorrerCarrito.
-let carritoPrestamos = [];
-let totalMontoPrestamos = 0;
-let totalMontoDevolucion = 0;
-let domCarrito = document.getElementById('carrito');
-let domBtnVaciar = document.getElementById('boton-vaciar'); 
+    let formu = document.getElementById('div1');
+    formu.classList.add('container');
+    formu.classList.add('row');
+    formu.classList.add('justify-content-around');
+    tablaCarrito();
+    agregarPlanesAlDom(formu);
+}
 
 
-// Creo tabla para carrito de préstamos.  
+/* Función para crear tabla donde mostrar los préstamos elegidos */
+function tablaCarrito() {
+    let domCarrito = document.getElementById('carrito');
+    let domBtnVaciar = document.getElementById('boton-vaciar'); 
+    tabla = document.createElement('table');
+    tabla.classList.add('table');
+    tabla.classList.add('table-hover');
+    tabla.innerHTML = `
+        <thead>
+            <tr>
+                <th>Préstamo</th>
+                <th>Capital</th>
+                <th>Interés</th>
+                <th>Cuotas</th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody></tbody>
+    `;
+    domCarrito.appendChild(tabla);
+}
 
-let tabla = document.createElement('table');
-tabla.classList.add('table');
-tabla.classList.add('table-hover');
-tabla.innerHTML = `
-    <thead>
-        <tr>
-            <th>Préstamo</th>
-            <th>Capital</th>
-            <th>Interés</th>
-            <th>Cuotas</th>
-            <th></th>
-        </tr>
-    </thead>
-    <tbody></tbody>
-`;
-domCarrito.appendChild(tabla);
+/* Función para agregar planes al Dom. Registrar la elección del préstamo. Mostrar sweetAlert */
+function agregarPlanesAlDom(formu) {
+    for(i = 0; i < datosAlmacenados.length; i ++) {
+        
+        const divPlan = document.createElement('div');
+        divPlan.classList.add('col-md-4');
+        divPlan.classList.add('cajitasPrestamos');
+        divPlan.classList.add('border');
+        divPlan.classList.add('border-success-subtle');
+        divPlan.textContent = datosAlmacenados[i].nombre;
+        divPlan.innerHTML = `
+                <h3>${datosAlmacenados[i].nombre}</h3>
+                <h3>$ ${datosAlmacenados[i].capital}</h3>
+                <h3>${datosAlmacenados[i].cuotas} cuotas</h3>
+                <h3>${datosAlmacenados[i].interes} % de interes mensual</h3>  
+            `;
+        
+        const btnPrestamos = document.createElement('button');
+        btnPrestamos.setAttribute('id',i);
+        btnPrestamos.setAttribute('marcador', i);
+        btnPrestamos.textContent = 'Seleccionar prestamo';
+        btnPrestamos.classList.add('btn');
+        btnPrestamos.classList.add('btn-outline-success');
+        btnPrestamos.addEventListener('click', (event) => {
+            event.preventDefault();
+            let prestamo = datosAlmacenados.find(p => p.id === parseInt(btnPrestamos.getAttribute('id')));
+            if((carritoPrestamos.find(c => c.id === prestamo.id) != null)) {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Préstamo ya elegido.',
+                    text: 'El sistema no habilita a elegir dos veces el mismo plan de préstamo.',      
+                });
+            } else {
+                if(carritoPrestamos.length < 2) {
+                    carritoPrestamos.push(prestamo);
+                    recorrerCarrito(carritoPrestamos);
+                } else {
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Límite superado',
+                        text: 'El sistema no habilita a elegir más de dos planes de dos planes',      
+                    });      
+                };              
+            };    
+            });
+        //Inserto las variables creadas.
+        divPlan.appendChild(btnPrestamos);
+        formu.appendChild(divPlan);
+    }
+}
 
-
-//Funcion para mostrarle al usuario la plata que va a tener que devolver por los planes elegidos.
+/* Función calculadora de préstamos */
 function recorrerCarrito(carritoPrestamos) {   
     totalMontoPrestamos = 0;
     totalMontoDevolucion = 0;
@@ -91,70 +146,11 @@ function recorrerCarrito(carritoPrestamos) {
         tbody.appendChild(fila);
     }
     localStorage.setItem('totalMontoDevolucion', totalMontoDevolucion);
+    const options = { style: "currency", currency: "ARS", minimumFractionDigits: 2, maximumFractionDigits: 2 };
     let mostrarTotal = document.getElementById('total');
-    mostrarTotal.textContent = totalMontoPrestamos;
+    mostrarTotal.textContent = totalMontoPrestamos.toLocaleString("es-AR", options);
     let mostrarTotalADevolver = document.getElementById('totalDevolucion');
-    mostrarTotalADevolver.textContent = totalMontoDevolucion;
+    mostrarTotalADevolver.textContent = totalMontoDevolucion.toLocaleString("es-AR", options);;
     let mostrarValorCuotaMensual = document.getElementById('totalCuotaDevolucion');
-    mostrarValorCuotaMensual.textContent = Math.round(totalMontoDevolucion / valorCuotaMensual);
-    //mostrarValorCuotaMensual.textContent = valorCuotaMensual;
+    mostrarValorCuotaMensual.textContent = Math.round(totalMontoDevolucion / valorCuotaMensual).toLocaleString("es-AR", options);
 }
-
-//Recorro el array con un bucle For y agrego los planes de préstamos en el HTML. 
-for(i = 0; i < datosAlmacenados.length; i ++) {
-    
-    const divPlan = document.createElement('div');
-    divPlan.classList.add('col-md-4');
-    divPlan.classList.add('cajitasPrestamos');
-    divPlan.classList.add('border');
-    divPlan.classList.add('border-success-subtle');
-    divPlan.textContent = datosAlmacenados[i].nombre;
-    divPlan.innerHTML = `
-            <h3>${datosAlmacenados[i].nombre}</h3>
-            <h3>$ ${datosAlmacenados[i].capital}</h3>
-            <h3>${datosAlmacenados[i].cuotas} cuotas</h3>
-            <h3>${datosAlmacenados[i].interes} % de interes mensual</h3>  
-        `;
-    
-    const btnPrestamos = document.createElement('button');
-    btnPrestamos.setAttribute('id',i);
-    btnPrestamos.setAttribute('marcador', i);
-    btnPrestamos.textContent = 'Seleccionar prestamo';
-    btnPrestamos.classList.add('btn');
-    btnPrestamos.classList.add('btn-outline-success');
-
-    //Creo el evento para el boton de Solicitar Prestamo. Le agrego IF para que no se pueda elegir mas
-    // de una vez cada prestamo. Le agrego un IF para que no se pueda tomar mas de dos prestamos.
-    //Llamo a la funcion recorrerCarrito.
-    btnPrestamos.addEventListener('click', (event) => {
-        event.preventDefault();
-        let prestamo = datosAlmacenados.find(p => p.id === parseInt(btnPrestamos.getAttribute('id')));
-        if((carritoPrestamos.find(c => c.id === prestamo.id) != null)) {
-            Swal.fire({
-                icon: 'error',
-                title: 'Préstamo ya elegido.',
-                text: 'El sistema no habilita a elegir dos veces el mismo plan de préstamo.',      
-            });
-        } else {
-            if(carritoPrestamos.length < 2) {
-                carritoPrestamos.push(prestamo);
-                recorrerCarrito(carritoPrestamos);
-                
-
-                
-            } else {
-                Swal.fire({
-                    icon: 'error',
-                    title: 'Límite superado',
-                    text: 'El sistema no habilita a elegir más de dos planes de dos planes',      
-                });      
-            };
-            
-        };    
-        });
-    //Inserto las variables creadas.
-    divPlan.appendChild(btnPrestamos);
-    formu.appendChild(divPlan);
-}
-}
-
